df68fbd2647c9d2e5a13660ef820cb4ab622f2f9
azoztiktok111-glitch <azoztiktok111@gmail.com>
2026-01-23 04:54:03 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { console.log("Service Worker مفعل ✅"); });
-self.addEventListener('push', event => {
-  const data = event.data ? event.data.text() : "تنبيه!";
-  event.waitUntil(
-    self.registration.showNotification("🕌 الصلاة", { body: data, icon: "icon.png" })
-  );
-});
